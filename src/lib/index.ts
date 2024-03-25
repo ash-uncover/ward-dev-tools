94b44e71a51cb67d464f1b77d876e9e838be6b5e
ash-uncover <ash.uncover@gmail.com>
2024-03-25 23:10:18 +0100

    Update index.ts

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,5 @@
 // Reexport your entry components here
-import '@ui5/webcomponents-icons/dist/Email.js'
+import '@ui5/webcomponents-icons/dist/email.js'
 
 import '@ui5/webcomponents/dist/Button.js'
 import '@ui5/webcomponents/dist/Label.js'
